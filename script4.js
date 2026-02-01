76c59d4c3368b5a5b1cccd77a9b394f421c96c10
ToSOvetS <dimaknr@gmail.com>
2026-02-01 15:49:44 +0300

    asd

--- script4.js
+++ script4.js
@@ -1,41 +1,42 @@
-let data = {};
-data.login = "MSHP1";
-data.theme = "dark";
-let data1 = {}
-data1.inf = [data];
-let dataJSON = JSON.stringify(data1);
-fetch('http://web4.informatics.ru:82/api/a0badc6d515368d213e8edad8fc39c76', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: dataJSON
+
+btn.addEventListener("click", () => {
+    document.querySelector(".error").innerText = "";
+    un = document.getElementById("username").value;
+    ps = document.getElementById("password").value;
+    console.log(cps);
+    if (! (fn && un && ps && cps)){
+        document.querySelector(".error").innerText = "Вы не заполнили все поля!";
+    } else{
+        fetch('https://cors-anywhere.herokuapp.com/http://web4.informatics.ru:82/api/a0badc6d515368d213e8edad8fc39c76')
+            .then(function(response) {
+                if (response.ok) {
+                return response.json();
+                } else {
+                console.log("Ошибка! Код: " + response.status);
+                }
+            })
+            .then(function(data) {
+                let fu = false;
+                let inde = 0;
+                let datas = data.inf;
+                console.log(datas);
+                for (let i = 0; i < datas.length; i++){
+                    if (un == datas[i]["login"]){
+                        inde = i;
+                        document.querySelector(".error").innerText = "Логин уже существует!";
+                        fu = true;
+                        break;
+                    }
+                }
+                if (fu){
+                    if (datas[i]["pass"] != ps){
+                        document.querySelector(".error").innerText = "Введен неверный павроль!"
+                    } else{
+                        localStorage.setItem("isLogged", true);
+                        localStorage.setItem("alls", JSON.stringify(data1));
+                        window.open("https://tosovets.github.io/halfyearproj/index.html");
+                    }
+                }
+            })
+    }
 })
-  .then(function(response) {
-    if (response.status === 201) {
-      console.log("Данные успешно отправлены!");
-      return response.json();
-    } else {
-      console.log("Ошибка! Код: " + response.status);
-    }
-  })
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(error) {
-    console.log("Ошибка соединения!");
-});
-fetch('http://web4.informatics.ru:82/api/a0badc6d515368d213e8edad8fc39c76')
-  .then(function(response) {
-    if (response.ok) {
-      return response.json();
-    } else {
-      console.log("Ошибка! Код: " + response.status);
-    }
-  })
-  .then(function(data) {
-    console.log("Логин пользователя: " + data.inf);
-  })
-  .catch(function(error) {
-    console.log("Ошибка соединения!");
-  });
