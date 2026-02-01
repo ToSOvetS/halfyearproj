ca9741c2ecaa7c6cbb228b3e69e4439b0ee4e1af
ToSOvetS <dimaknr@gmail.com>
2026-02-01 16:08:29 +0300

    asd

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let flows = [];
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://tosovets.github.io/halfyearproj/data.json', false);
-
-try {
-  xhr.send();
-  if (xhr.status === 200) {
-    let data = JSON.parse(xhr.responseText);
-    flows = data;
-    console.log("Логин пользователя: ");
-  } else {
-    console.log("Ошибка! Статус: " + xhr.status);
-  }
-} catch (error) {
-  console.log("Произошла сетевая ошибка, данные не получены.");
-}
+let flows = JSON.parse(localStorage.getItem("alls"))["flowrs"];
